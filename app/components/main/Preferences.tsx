7c70c56121fc1b7e86fb982ee2c1d76bdfc898b0
windopper <open0916@naver.com>
2025-08-08 01:00:42 +0900

    refactor: simplify Preferences component by removing background styles and labels for technology icons

--- app/components/main/Preferences.tsx
+++ app/components/main/Preferences.tsx
@@ -33,82 +33,63 @@ export default function Preferences() {
       </div>
       <div className="flex flex-wrap gap-4 justify-center">
         {/* Frontend */}
-        <div className="flex flex-col items-center gap-2 p-3 rounded-lg bg-blue-500/10 border border-blue-500/20">
+        <div className="flex flex-col items-center gap-2 p-3">
           <SiTypescript className="w-8 h-8 text-blue-500" />
-          <span className="text-xs">TypeScript</span>
         </div>
-        <div className="flex flex-col items-center gap-2 p-3 rounded-lg bg-blue-400/10 border border-blue-400/20">
+        <div className="flex flex-col items-center gap-2 p-3">
           <FaPython className="w-8 h-8 text-blue-400" />
-          <span className="text-xs">Python</span>
         </div>
 
-        <div className="flex flex-col items-center gap-2 p-3 rounded-lg bg-yellow-500/10 border border-yellow-500/20">
+        <div className="flex flex-col items-center gap-2 p-3">
           <FaJs className="w-8 h-8 text-yellow-500" />
-          <span className="text-xs">JavaScript</span>
         </div>
-        <div className="flex flex-col items-center gap-2 p-3 rounded-lg bg-cyan-500/10 border border-cyan-500/20">
+        <div className="flex flex-col items-center gap-2 p-3">
           <FaReact className="w-8 h-8 text-cyan-500" />
-          <span className="text-xs">React</span>
         </div>
-        <div className="flex flex-col items-center gap-2 p-3 rounded-lg bg-black/10 border border-gray-500/20">
+        <div className="flex flex-col items-center gap-2 p-3">
           <SiNextdotjs className="w-8 h-8 text-white" />
-          <span className="text-xs">Next.js</span>
         </div>
-        <div className="flex flex-col items-center gap-2 p-3 rounded-lg bg-purple-500/10 border border-purple-500/20">
+        <div className="flex flex-col items-center gap-2 p-3">
           <SiRedux className="w-8 h-8 text-purple-500" />
-          <span className="text-xs">Redux</span>
         </div>
-        <div className="flex flex-col items-center gap-2 p-3 rounded-lg bg-green-500/10 border border-green-500/20">
+        <div className="flex flex-col items-center gap-2 p-3">
           <TbBrandThreejs className="w-8 h-8 text-green-500" />
-          <span className="text-xs">Three.js</span>
         </div>
-        <div className="flex flex-col items-center gap-2 p-3 rounded-lg bg-gray-500/10 border border-gray-500/20">
+        <div className="flex flex-col items-center gap-2 p-3">
           <SiSwr className="w-8 h-8 text-gray-400" />
-          <span className="text-xs">SWR</span>
         </div>
-        <div className="flex flex-col items-center gap-2 p-3 rounded-lg bg-[#009688]/10 border border-[#009688]/20">
+        <div className="flex flex-col items-center gap-2 p-3">
           <SiFastapi className="w-8 h-8 text-[#009688]" />
-          <span className="text-xs">FastAPI</span>
         </div>
-        <div className="flex flex-col items-center gap-2 p-3 rounded-lg bg-cyan-400/10 border border-cyan-400/20">
+        <div className="flex flex-col items-center gap-2 p-3">
           <SiTailwindcss className="w-8 h-8 text-cyan-400" />
-          <span className="text-xs">Tailwind</span>
         </div>
-        <div className="flex flex-col items-center gap-2 p-3 rounded-lg bg-orange-500/10 border border-orange-500/20">
+        <div className="flex flex-col items-center gap-2 p-3">
           <SiD3Dotjs className="w-8 h-8 text-orange-500" />
-          <span className="text-xs">D3.js</span>
         </div>
-        <div className="flex flex-col items-center gap-2 p-3 rounded-lg bg-[#2088FF]/10 border border-[#2088FF]/20">
+        <div className="flex flex-col items-center gap-2 p-3">
           <SiGithubactions className="w-8 h-8" color="#2088FF" />
-          <span className="text-xs">Github Action</span>
         </div>
-        <div className="flex flex-col items-center gap-2 p-3 rounded-lg bg-orange-500/10 border border-orange-500/20">
+        <div className="flex flex-col items-center gap-2 p-3">
           <SiCloudflare className="w-8 h-8 text-orange-500" />
-          <span className="text-xs">Cloudflare</span>
         </div>
-        <div className="flex flex-col items-center gap-2 p-3 rounded-lg bg-blue-600/10 border border-blue-600/20">
+        <div className="flex flex-col items-center gap-2 p-3">
           <SiGooglecloud className="w-8 h-8 text-blue-600" />
-          <span className="text-xs">GCP</span>
         </div>
-        <div className="flex flex-col items-center gap-2 p-3 rounded-lg bg-black/10 border border-gray-500/20">
+        <div className="flex flex-col items-center gap-2 p-3">
           <SiVercel className="w-8 h-8 text-white" />
-          <span className="text-xs">Vercel</span>
         </div>
-        <div className="flex flex-col items-center gap-2 p-3 rounded-lg bg-[#C5F74F]/10 border border-[#C5F74F]/20">
+        <div className="flex flex-col items-center gap-2 p-3">
           <SiDrizzle className="w-8 h-8" color="#C5F74F" />
-          <span className="text-xs">Drizzle</span>
         </div>
-        <div className="flex flex-col items-center gap-2 p-3 rounded-lg bg-blue-500/10 border border-blue-500/20">
+        <div className="flex flex-col items-center gap-2 p-3">
           <SiDocker className="w-8 h-8 text-blue-500" />
-          <span className="text-xs">Docker</span>
         </div>
-        <div className="flex flex-col items-center gap-2 p-3 rounded-lg bg-red-500/10 border border-red-500/20">
+        <div className="flex flex-col items-center gap-2 p-3">
           <SiGit className="w-8 h-8 text-red-500" />
-          <span className="text-xs">Git</span>
         </div>
-        <div className="flex flex-col items-center gap-2 p-3 rounded-lg bg-[#1C3C3C]/10 border border-[#1C3C3C]/20">
+        <div className="flex flex-col items-center gap-2 p-3">
           <SiLangchain className="w-8 h-8" color="#1C3C3C" />
-          <span className="text-xs">Langchain</span>
         </div>
       </div>
     </div>
