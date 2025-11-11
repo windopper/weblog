82a8f42c0b34945aa2fe3cb2b4e8da1992ac59e4
windopper <sevenstarpleiades@gmail.com>
2025-11-11 13:49:59 +0900

    feat: integrate memo list into homepage and enhance layout for recent posts and memos

--- app/components/main/Preferences.tsx
+++ app/components/main/Preferences.tsx
@@ -1,55 +1,69 @@
-import { 
-    FaGithub, 
-    FaReact, 
-    FaPython, 
-    FaJs, 
-    FaNodeJs 
-  } from "react-icons/fa";
-  import { 
-    SiTypescript, 
-    SiNextdotjs, 
-    SiRedux, 
-    SiThreedotjs, 
-    SiSwr, 
-    SiTailwindcss, 
-    SiCloudflare, 
-    SiGooglecloud, 
-    SiVercel, 
-    SiDocker, 
-    SiGit, 
-    SiD3Dotjs,
-    SiFastapi,
-    SiDrizzle,
-    SiLangchain,
-    SiGithubactions,
-    SiUnrealengine,
-    SiCplusplus
-  } from "react-icons/si";
+import { FaGithub, FaReact, FaPython, FaJs, FaNodeJs } from "react-icons/fa";
+import {
+  SiTypescript,
+  SiNextdotjs,
+  SiRedux,
+  SiThreedotjs,
+  SiSwr,
+  SiTailwindcss,
+  SiCloudflare,
+  SiGooglecloud,
+  SiVercel,
+  SiDocker,
+  SiGit,
+  SiD3Dotjs,
+  SiFastapi,
+  SiDrizzle,
+  SiLangchain,
+  SiGithubactions,
+  SiUnrealengine,
+  SiCplusplus,
+} from "react-icons/si";
 import { TbBrandThreejs } from "react-icons/tb";
 
 export default function Preferences() {
   return (
-    <div className="flex flex-col w-full z-50 max-w-4xl">
+    <div className="flex flex-col w-full max-w-4xl">
       <div className="flex flex-row justify-between items-center text-sm ">
         <span className="font-bold py-4">선호 기술</span>
       </div>
       <div className="flex flex-wrap gap-4 justify-center">
-        <PreferenceItem icon={<SiTypescript className="w-8 h-8 text-blue-500" />} title="TypeScript" />
-        <PreferenceItem icon={<FaPython className="w-8 h-8 text-blue-400" />} title="Python" />
-        <PreferenceItem icon={<SiCplusplus className="w-8 h-8 text-blue-600" />} title="C++" />
-        
-        <PreferenceItem icon={<FaReact className="w-8 h-8 text-cyan-500" />} title="React" />
-        <PreferenceItem icon={<SiNextdotjs className="w-8 h-8 text-white" />} title="Next.js" />
-        <PreferenceItem icon={<SiUnrealengine className="w-8 h-8 text-white" />} title="Unreal Engine" />
+        <PreferenceItem
+          icon={<SiTypescript className="w-8 h-8 text-blue-500" />}
+          title="TypeScript"
+        />
+        <PreferenceItem
+          icon={<FaPython className="w-8 h-8 text-blue-400" />}
+          title="Python"
+        />
+        <PreferenceItem
+          icon={<SiCplusplus className="w-8 h-8 text-blue-600" />}
+          title="C++"
+        />
+
+        <PreferenceItem
+          icon={<FaReact className="w-8 h-8 text-cyan-500" />}
+          title="React"
+        />
+        <PreferenceItem
+          icon={<SiNextdotjs className="w-8 h-8 text-white" />}
+          title="Next.js"
+        />
+        <PreferenceItem
+          icon={<SiUnrealengine className="w-8 h-8 text-white" />}
+          title="Unreal Engine"
+        />
       </div>
     </div>
   );
 }
 
-function PreferenceItem({ icon, title }: { icon: React.ReactNode, title: string }) {
-  return (
-    <div className="flex flex-col items-center gap-2 p-3">
-      {icon}
-    </div>
-  );
+function PreferenceItem({
+  icon,
+  title,
+}: {
+  icon: React.ReactNode;
+  title: string;
+}) {
+  return <div className="flex flex-col items-center gap-2 p-3">{icon}</div>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,8 @@ import { FaGithub } from "react-icons/fa";
 import TakeoffButton from "./components/main/TakeoffButton";
 import { Blog, WithContext } from "schema-dts";
 import PleroButton from "./components/main/PleroButton";
+import { getMemoFiles } from "./action/memo";
+import MemoList from "./components/memos/MemoList";
 
 const jsonLdData: WithContext<Blog> = {
   "@context": "https://schema.org",
@@ -16,15 +18,14 @@ const jsonLdData: WithContext<Blog> = {
   "@id": "https://kamilereon.net",
   mainEntityOfPage: "https://kamilereon.net",
   name: "kamilereon 기술 블로그",
-  description: "웹 개발, AI 기술, 소프트웨어 아키텍처에 대한 기술 블로그입니다. React, Next.js, AI 기술 등 다양한 개발 경험을 공유합니다.",
+  description:
+    "웹 개발, AI 기술, 소프트웨어 아키텍처에 대한 기술 블로그입니다. React, Next.js, AI 기술 등 다양한 개발 경험을 공유합니다.",
   url: "https://kamilereon.net",
   author: {
     "@type": "Person",
     name: "kamilereon",
     url: "https://github.com/windopper",
-    sameAs: [
-      "https://github.com/windopper"
-    ]
+    sameAs: ["https://github.com/windopper"],
   },
   publisher: {
     "@type": "Organization",
@@ -33,14 +34,16 @@ const jsonLdData: WithContext<Blog> = {
       "@type": "ImageObject",
       url: "https://kamilereon.net/image/weblog.png",
       width: "512",
-      height: "512"
-    }
+      height: "512",
+    },
   },
-  inLanguage: "ko-KR"
-}
+  inLanguage: "ko-KR",
+};
 
 export default async function Home() {
   const markdownFiles = await getMarkdownFiles();
+  const memos = await getMemoFiles();
+  const latest4Memos = memos.slice(0, 4);
   const publicMarkdownFiles = markdownFiles.filter((file) => !file.isPrivate);
   const latest4PublicMarkdownFiles = publicMarkdownFiles.slice(0, 4);
 
@@ -61,17 +64,27 @@ export default async function Home() {
           </Link>
         </div>
       </div>
+
       {/* Content */}
       <div className="flex flex-col z-50 w-full max-w-4xl">
         <div className="flex flex-row justify-between items-center text-sm ">
           <span className="font-bold py-4">최근 게시물</span>
-          <Link href="/posts" title="최근 게시물">더보기</Link>
+          <Link href="/posts" title="최근 게시물">
+            더보기
+          </Link>
         </div>
         <PostsList markdownFiles={latest4PublicMarkdownFiles} type="small" />
       </div>
 
+      <div className="flex flex-col z-50 w-full max-w-4xl">
+        <div className="flex flex-row justify-between items-center text-sm ">
+          <span className="font-bold py-4">최근 메모</span>
+        </div>
+        <MemoList memos={latest4Memos} />
+      </div>
+
       {/* 토이 프로젝트 */}
-      <div className="flex flex-col w-full z-50 max-w-4xl">
+      <div className="flex flex-col w-full max-w-4xl">
         <div className="flex flex-row justify-between items-center text-sm ">
           <span className="font-bold py-4">토이 프로젝트</span>
         </div>
